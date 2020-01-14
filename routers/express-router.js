00eab13c6cf50e16d1a8893dcc95845761a54d1f
taterntots <matthew.a.weidner@gmail.com>
2020-01-14 17:00:26 -0500

    fulfilled the requirements for my put, allowing me to edit any posts with a specified id.

--- routers/express-router.js
+++ routers/express-router.js
@@ -50,4 +50,27 @@ router.get('/:id', (req, res) => {
         })
 })
 
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+
+    if (!changes.title || !changes.contents) {
+        res.status(400).json({ errorMessage: 'Please provide title and contents for the post' });
+    } else {
+        Database.update(id, changes)
+            .then(post => {
+                if (post) {
+                    console.log('post edited', post);
+                    res.status(200).json(post);
+                } else {
+                    res.status(404).json({ errorMessage: 'The post with the specified ID does not exist' });
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).json({ errorMessage: 'The post information could not be modified' });
+            })
+    }
+})
+
 module.exports = router;
